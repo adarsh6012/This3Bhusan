b2fd83c496e930276cf96726b0b869f367eac514
Adarsh Kumar <70280399+adarsh6012@users.noreply.github.com>
2021-07-12 23:23:41 +0530

    Add files via upload

---
+++ ClientThread.java
@@ -0,0 +1,106 @@
+
+import java.awt.Color;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.text.DecimalFormat;
+import java.util.StringTokenizer;
+import java.util.Vector;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class ClientThread implements Runnable{
+    
+    Socket socket;
+    DataInputStream dis;
+    DataOutputStream dos;
+    MainForm main;
+    StringTokenizer st;
+    protected DecimalFormat df = new DecimalFormat("##,#00");
+    
+    public ClientThread(Socket socket, MainForm main){
+        this.main = main;
+        this.socket = socket;
+        try {
+            dis = new DataInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            main.appendMessage("[IOException]: "+ e.getMessage(), "Error", Color.RED, Color.RED);
+        }
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            while(!Thread.currentThread().isInterrupted()){
+                String data = dis.readUTF();
+                st = new StringTokenizer(data);
+                String CMD = st.nextToken();
+                switch(CMD){
+                    case "CMD_MESSAGE":
+                        SoundEffect.MessageReceive.play();
+                        String msg = "";
+                        String frm = st.nextToken();
+                        while(st.hasMoreTokens()){
+                            msg = msg +" "+ st.nextToken();
+                        }
+                        main.appendMessage(msg, frm, Color.MAGENTA, Color.BLUE);
+                        break;
+                        
+                    case "CMD_ONLINE":
+                        Vector online = new Vector();
+                        while(st.hasMoreTokens()){
+                            String list = st.nextToken();
+                            if(!list.equalsIgnoreCase(main.username)){
+                                online.add(list);
+                            }
+                        }
+                        main.appendOnlineList(online);
+                        break;
+                     
+                    case "CMD_FILE_XD":
+                        String sender = st.nextToken();
+                        String receiver = st.nextToken();
+                        String fname = st.nextToken();
+                        int confirm = JOptionPane.showConfirmDialog(main, "From: "+sender+"\nFile Name: "+fname+"\nDo you accept this file?");
+                        if(confirm == 0){
+                            main.openFolder();
+                            try {
+                                dos = new DataOutputStream(socket.getOutputStream());
+                                String format = "CMD_SEND_FILE_ACCEPT "+sender+" Accept";
+                                dos.writeUTF(format);
+                                Socket fSoc = new Socket(main.getMyHost(), main.getMyPort());
+                                DataOutputStream fdos = new DataOutputStream(fSoc.getOutputStream());
+                                fdos.writeUTF("CMD_SHARINGSOCKET "+ main.getMyUsername());
+                                new Thread(new ReceivingFileThread(fSoc, main)).start();
+                            } catch (IOException e) {
+                                System.out.println("[CMD_FILE_XD]: "+e.getMessage());
+                            }
+                        } else {
+                            try {
+                                dos = new DataOutputStream(socket.getOutputStream());
+                                String format = "CMD_SEND_FILE_ERROR "+sender+" User denied your request or lost connection.!";
+                                dos.writeUTF(format);
+                            } catch (IOException e) {
+                                System.out.println("[CMD_FILE_XD]: "+e.getMessage());
+                            }
+                        }                       
+                        break;   
+                        
+                    default: 
+                        main.appendMessage("[CMDException]: Unknown command "+ CMD, "CMDException", Color.RED, Color.RED);
+                    break;
+                }
+            }
+        } catch(IOException e){
+            main.appendMessage(" Lost connection to the Client, please try again.!", "Error", Color.RED, Color.RED);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # This3Bhusan
+File Sharing Application
+
+This Application allows file sharing between mutiples nodes.
+
+## Server : Start-Up Page
+
+![Screenshot 2020-07-23 at 7 51 09 PM](https://user-images.githubusercontent.com/54954596/88314534-af389000-cd32-11ea-8586-97034f3d5513.png)
+
+
+## Server : Start the Server
+
+![Screenshot 2020-07-23 at 8 07 18 PM](https://user-images.githubusercontent.com/54954596/88314711-ef980e00-cd32-11ea-9c47-6555569b1940.png)
+
+
+## Server : Stop Server
+
+![Screenshot 2020-07-23 at 10 23 11 PM](https://user-images.githubusercontent.com/54954596/88314813-1f471600-cd33-11ea-896e-f654fc0e552c.png)
+
+
+## Client : Start-Up Page
+
+![Screenshot 2020-07-23 at 10 29 10 PM](https://user-images.githubusercontent.com/54954596/88315934-a34dcd80-cd34-11ea-8580-7bf7c1856a31.png)
+
+
+## Client : Log In Page
+
+![Screenshot 2020-07-23 at 10 30 00 PM](https://user-images.githubusercontent.com/54954596/88315945-a648be00-cd34-11ea-9617-f10752bb1ef6.png)
+
+
+## Client : Chat Window
+
+![Screenshot 2020-07-23 at 10 30 21 PM](https://user-images.githubusercontent.com/54954596/88315953-aa74db80-cd34-11ea-9793-147a50eff184.png)
+
+
+## Server : Data Access
+
+![Screenshot 2020-07-23 at 10 30 24 PM](https://user-images.githubusercontent.com/54954596/88315964-af398f80-cd34-11ea-84b0-3b7bba69b2dd.png)
+
+
+## Client : File Sharing Window
+
+![Screenshot 2020-07-23 at 10 31 41 PM](https://user-images.githubusercontent.com/54954596/88315975-b2348000-cd34-11ea-8605-d17a71406c5b.png)
+
+## Client : File Browser
+
+![Screenshot 2020-07-25 at 6 47 07 PM](https://user-images.githubusercontent.com/54954596/88458241-47a55080-ceaa-11ea-89af-9945918b3311.png)
+
+
+## Client : Confirm Dialog
+
+![Screenshot 2020-07-25 at 6 47 29 PM](https://user-images.githubusercontent.com/54954596/88458261-70c5e100-ceaa-11ea-96f6-ef6de81eeb54.png)
+
+
+## Client : File Browser (Recieve)
+
+![Screenshot 2020-07-25 at 6 47 36 PM](https://user-images.githubusercontent.com/54954596/88458263-73283b00-ceaa-11ea-9581-1b9dc060bcc0.png)
+
+
+## Client : Success Dialog
+
+![Screenshot 2020-07-25 at 6 47 51 PM](https://user-images.githubusercontent.com/54954596/88458264-74f1fe80-ceaa-11ea-85d0-28ede2adea76.png)
